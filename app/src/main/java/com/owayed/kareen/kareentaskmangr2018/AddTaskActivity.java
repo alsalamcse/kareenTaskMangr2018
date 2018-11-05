6f19e4a4d213dcf3f71a9334de53d1295a7884ff
user <shada.hsarme@gmail.com>
2018-11-05 14:07:44 +0200

    1.splash screen is okay, and added main activity taps. next step: bulding login, sign up screens.

--- app/src/main/java/com/owayed/kareen/kareentaskmangr2018/AddTaskActivity.java
+++ app/src/main/java/com/owayed/kareen/kareentaskmangr2018/AddTaskActivity.java
@@ -1,19 +1,24 @@
 package com.owayed.kareen.kareentaskmangr2018;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddTaskActivity extends AppCompatActivity {
     private EditText etTitle,etText,etDate;
     private TextView tvImportant,tvNecessary;
     private SeekBar skbrImportant,skbrNecessary;
     private Button btSave,btnDatePicker;
+    private int mYear,mMonth,mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +35,45 @@ public class AddTaskActivity extends AppCompatActivity {
         btSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AddTaskActivity.this, MainTapsActivity.class);
-                startActivity(intent);
 
-            }
-        });
-        btnDatePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i2 = new Intent(AddTaskActivity.this, LogInActivity.class);
-                startActivity(i2);
+                dataHandler();
 
             }
         });
     }
+    private void dataHandler(){
+
+        boolean isok = true;
+        String title= etTitle.getText().toString();
+        String text = etText.getText().toString();
+        String date = etDate.getText().toString();
+        String important=tvImportant.getText().toString();
+        String necessary=tvNecessary.getText().toString();
+
+        if (title.length() < 4 || title.indexOf('@') < 0 || title.indexOf('.') < 0) {
+            etTitle.setError("Wrong Email");
+            isok = false;
+        }
+        if (text.length() < 8) {
+           etText.setError("Have to be at least 8 char");
+            isok = false;
+        }
+
+    }
+    public void onClick(View view){
+        if (view==btnDatePicker){
+            final Calendar c=Calendar.getInstance();
+            mYear=c.get(Calendar.YEAR);
+            mMonth=c.get(Calendar.MONTH);
+            mDay=c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                    etDate.setText(mDay+"-"+mMonth+"-"+mYear).show;
+                }
+            })
+        }
+    }
+
+
 }
