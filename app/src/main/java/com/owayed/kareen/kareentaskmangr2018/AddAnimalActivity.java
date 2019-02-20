6723de330534cc7f7e9fdb36018a9fef3e75d018
kareenow <kareenowayed@gmail.com>
2019-02-20 08:53:21 +0200

    new

--- app/src/main/java/com/owayed/kareen/kareentaskmangr2018/AddAnimalActivity.java
+++ app/src/main/java/com/owayed/kareen/kareentaskmangr2018/AddAnimalActivity.java
@@ -2,12 +2,32 @@ package com.owayed.kareen.kareentaskmangr2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddAnimalActivity extends AppCompatActivity {
+    private EditText etType,etAge,etColor,etAddress,etMoney;
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_animal);
+        etType=findViewById(R.id.etType);
+        etAge=findViewById(R.id.etAge);
+        etColor=findViewById(R.id.etColor);
+        etAddress=findViewById(R.id.etAddress);
+        etMoney=findViewById(R.id.etMoney);
+        btnSave=findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
